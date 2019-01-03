172e7b8aedc228eac3f6146a808de812090ac3d1
Angela Wolff <warriorwomenblog@gmail.com>
2019-01-03 18:46:33 +0000

    Adding colorpicker background selection to frontend. No persistence

---
+++ app/javascript/components/Colorpicker.js
@@ -0,0 +1,36 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+import { BlockPicker } from "react-color"
+
+class Colorpicker extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { selector: props.selector };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange = color => {
+    document.body.style.background = color.hex;
+    let elem = document.querySelector('#'+this.state.selector)
+  }
+
+  handleChange = (color) => {
+    document.body.style.background = color.hex;
+    let elem = document.querySelector('#'+this.state.selector)
+    elem.value = color.hex;
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <BlockPicker color = { this.props.color }
+                     onChange = { this.handleChange }/>;
+      </React.Fragment>
+    );
+  }
+}
+
+Colorpicker.propTypes = {}
+
+export default Colorpicker
